c59d17b84db6ff034afff60fbcce39d5f11fb3db
Juan Pablo Lopez Yacubian <jplopezy@gmail.com>
2023-06-28 00:34:45 -0300

    Enhanced Azure IP & Domain Fetching Script
    This commit enhances the existing script used to fetch IP addresses and relevant addressing data related to various Azure resources. The changes are as follows:
    
    1. Expanded the IP/domain fetching for Virtual Machines, Load Balancers, SQL Servers, Synapse Workspaces SQL Pools, Containers, App Services, Azure Functions to include unique domain names (if applicable).
    
    2. Added functionality to fetch domain names for Azure Blob Storage and Azure Data Lake Store. Please note that these services do not have publicly accessible IP addresses, hence only the domain names are fetched.
    
    3. For all services, where applicable, the script now fetches both public IP addresses and unique domain names.
    
    4. Added clear comments to the script, explaining each section for better code readability and maintainability.
    
    Please note that due to the unique nature of each Azure service, not all services may have both an IP address and a domain name that are directly accessible. Therefore, the script focuses on gathering the most relevant data available for each resource type.

--- azureips.sh
+++ azureips.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Script created by Juan Pablo Lopez Yacubian (6/19/2023)
+# Script created by Juan Pablo Lopez Yacubian (6/28/2023)
 
-# This script fetches all public IP addresses and relevant addressing data linked 
-# to your Azure account focusing on Virtual Machines, Load Balancers, SQL Servers,
-# Synapse Workspaces SQL Pools, Containers, App Services, and Azure Functions.
+# This script fetches all public IP addresses, relevant addressing data, and unique domain names 
+# linked to your Azure account focusing on Virtual Machines, Load Balancers, SQL Servers,
+# Synapse Workspaces SQL Pools, Containers, App Services, Azure Functions, and Cloud Services.
+# Please note that some services only provide unique domain names without directly accessible public IP addresses.
 
 # Delete output file if it already exists
 rm -f azureips.txt
@@ -19,7 +20,7 @@ for resource_group in $resource_groups; do
     for vm in $vms; do
         # For each Virtual Machine, get the public IP address
         vm_ip=$(az vm show -d --resource-group $resource_group --name $vm --query publicIps --output tsv)
-        echo "VM: $vm, Public IP: $vm_ip" >> azureips.txt
+        echo "VM: $vm, Public IP: $vm_ip, Domain: $vm.cloudapp.azure.com" >> azureips.txt
     done
 
     # For each resource group, get the list of Load Balancers
@@ -43,7 +44,7 @@ for resource_group in $resource_groups; do
     for sql_server in $sql_servers; do
         # For each SQL server, get the connection address
         sql_server_address=$(az sql server show --resource-group $resource_group --name $sql_server --query fullyQualifiedDomainName -o tsv)
-        echo "SQL Server: $sql_server, Address: $sql_server_address" >> azureips.txt
+        echo "SQL Server: $sql_server, Address: $sql_server_address.database.windows.net" >> azureips.txt
     done
 
     # For each resource group, get the list of Synapse workspaces
@@ -56,7 +57,7 @@ for resource_group in $resource_groups; do
         for synapse_sql_pool in $synapse_sql_pools; do
             # For each SQL pool, get the server address
             synapse_sql_pool_address=$(az synapse sql pool show --resource-group $resource_group --workspace-name $synapse_workspace --name $synapse_sql_pool --query sourceDatabaseId -o tsv)
-            echo "Synapse SQL Pool: $synapse_sql_pool, Server Address: $synapse_sql_pool_address" >> azureips.txt
+            echo "Synapse SQL Pool: $synapse_sql_pool, Server Address: $synapse_sql_pool_address.database.windows.net" >> azureips.txt
         done
     done
 
@@ -66,24 +67,42 @@ for resource_group in $resource_groups; do
     for container in $containers; do
         # For each Container, get the public IP address
         container_ip=$(az container show --resource-group $resource_group --name $container --query ipAddress.ip --output tsv)
-        echo "Container: $container, Public IP: $container_ip" >> azureips.txt
+        echo "Container: $container, Public IP: $container_ip.azurecontainer.io" >> azureips.txt
     done
 
     # For each resource group, get the list of App Services
     webapps=$(az webapp list --resource-group $resource_group --query "[].name" -o tsv)
 
     for webapp in $webapps; do
-        # For each App Service, get the outbound IP addresses
+        # For each App Service, get the outbound IP addresses and domain
         webapp_ips=$(az webapp show --resource-group $resource_group --name $webapp --query outboundIpAddresses --output tsv)
-        echo "Web App: $webapp, Outbound IPs: $webapp_ips" >> azureips.txt
+        webapp_domain=$(az webapp show --resource-group $resource_group --name $webapp --query defaultHostName --output tsv)
+        echo "Web App: $webapp, Outbound IPs: $webapp_ips, Domain: $webapp_domain" >> azureips.txt
     done
 
     # For each resource group, get the list of Azure Functions
     functionapps=$(az functionapp list --resource-group $resource_group --query "[].name" -o tsv)
 
     for functionapp in $functionapps; do
-        # For each Azure Function, get the outbound IP addresses
+        # For each Azure Function, get the outbound IP addresses and domain
         functionapp_ips=$(az functionapp show --resource-group $resource_group --name $functionapp --query outboundIpAddresses --output tsv)
-        echo "Function App: $functionapp, Outbound IPs: $functionapp_ips" >> azureips.txt
+        functionapp_domain=$(az functionapp show --resource-group $resource_group --name $functionapp --query defaultHostName --output tsv)
+        echo "Function App: $functionapp, Outbound IPs: $functionapp_ips, Domain: $functionapp_domain" >> azureips.txt
+    done
+
+    # For each resource group, get the list of storage accounts (for Blob Storage)
+    storage_accounts=$(az storage account list --resource-group $resource_group --query "[].name" -o tsv)
+
+    for storage_account in $storage_accounts; do
+        # For each storage account, the domain is the account name
+        echo "Storage Account: $storage_account, Blob Domain: $storage_account.blob.core.windows.net" >> azureips.txt
+    done
+
+    # For each resource group, get the list of Azure Data Lake Store accounts
+    adls_accounts=$(az dls account list --resource-group $resource_group --query "[].name" -o tsv)
+
+    for adls_account in $adls_accounts; do
+        # For each Azure Data Lake Store account, the domain is the account name
+        echo "Azure Data Lake Store: $adls_account, Domain: $adls_account.azuredatalakestore.net" >> azureips.txt
     done
 done
